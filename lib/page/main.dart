9725b1c2323048445b0b57b2c13690807797b2ef
JangYeonghun <gnsdudwkd@naver.com>
2023-07-28 12:04:51 +0900

    사물인식 1차 구현

--- lib/page/main.dart
+++ lib/page/main.dart
@@ -1,28 +1,35 @@
 /*
 개발자: 장영훈
-2023.07.25 ver.1 bottom_bar_class 연결, 네이버지도 연결, 메인 페이지 제작
+2023.07.25 ver.1 bottom_bar_class 연결, 네이버지도 연결, 메인 페이지 제작, 네이버지도 초기화 추가
+2023.07.27 ver.2 detecting_frame 연결, 카메라 초기화 추가
  */
 
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:blind_support/class/bottom_bar_class.dart';
+import 'package:blind_support/components/bottom_bar_widget/bottom_bar_class.dart';
 import 'package:blind_support/components//detecting_widget/detecting_frame.dart';
 import 'package:blind_support/components//map_widget/map_frame.dart';
 import 'package:blind_support/components//weather_widget/weather_frame.dart';
 import 'package:blind_support/utility/tts.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 
-
+late List<CameraDescription> cameras;
 // MVC 패턴 중 View
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await NaverMapSdk.instance.initialize(
-    clientId: "z68p1f0nht",
-    onAuthFailed: (ex) {
-      print("********* 네이버맵 인증오류 : $ex *********");
-    });
+  // 네이버지도 초기화를 위해 호출
   // 401: 잘못된 클라이언트 ID 지정, 잘못된 클라이언트 유형, 콘솔에 등록된 앱 패키지 이름과 미일치
   // 429: 콘솔에서 Maps 서비스를 선택하지 않음, 사용 한도 초과
   // 800: 클라이언트 ID 미지정
+  WidgetsFlutterBinding.ensureInitialized();
+  await NaverMapSdk.instance.initialize(
+      clientId: "z68p1f0nht",
+      onAuthFailed: (ex) {
+        print("********* 네이버맵 인증오류 : $ex *********");
+      });
+
+  // 카메라 초기화를 위해 호출
+  cameras = await availableCameras();
+
   runApp(const MyApp());
 }
 
@@ -61,7 +68,7 @@ class _MyAppState extends State<MyApp> {
         return WeatherFrame();
       case 1:
       // 사물인식 화면
-        return DetectingFrame();
+        return DetectingFrame(cameras: cameras);
       case 2:
       // 길 찾기 화면
         return DirectionsFrame();
